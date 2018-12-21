b6046c4305b49b3930d33dad5acacba096d7d238
m1heng <miheng@me.com>
2018-12-22 06:12:45 +0800

    auth part finished, chinese tested, userinfo finished. Considering to combine user table and userinfo table

--- api.py
+++ api.py
@@ -50,18 +50,27 @@ class SRP(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable =False)
     km_id = db.Column(db.String(50), unique = True, nullable = False)
-    decision = db.(db.Boolean)
+    decision = db.Column(db.Boolean)
     amount = db.Column(db.Integer)
     request_reason = db.Column(db.String(255))
     decision_reason = db.Column(db.String(255))
 
 class FleetRecord(db.Model):
+    __tablename__  = 'FleetRecord'
     id = db.Column(db.Integer, primary_key = True)
-    fc_char_id = db.Column(db.String(50), nullable = False)
-    
+    fc_user_id = db.Column(db.String(50), nullable = False)
+    fleet_doctrine = db.Column(db.String(50), nullable = False)
+
+class FleetMemberRecord(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    fleet_id = db.Column(db.Integer, db.ForeignKey('FleetRecord.id'), nullable = False)
+    char_id  = db.Column(db.String(50), nullable = False)
+    ship_type = db.Column(db.String(50), nullable = False)
+
 
 '''
-decorator for login required. Normal User role
+decorator for login required. Normal User role.
+current_user must be added as a parameter to wrapped function
 '''
 def login_required(f):
     @wraps(f)
@@ -129,33 +138,100 @@ def register():
         db.session.commit()
         return jsonify({'message': 'Successfully registered'})
     except Exception as e:
-        return jsonify({'message': str(e)}), 401
+        return jsonify({'message': str(e)}), 403
 
-@app.route('/auth/resetpassword', methods=['POST'])
-def resetPassword:
-    return ''
+'''
+reset password with reenter current password. 
+Login required.
+'''
+@app.route('/auth/resetpassword', methods=['PUT'])
+@login_required
+def resetPassword(current_user):
+    data = request.get_json()
+    current_password = data['current_password']
+    new_password = data['new_password']
 
-@app.route('/auth/updateRole', methods=['UPDATE'])
-def updateRole():
-    return ''
+    if not check_password_hash(current_user.hashed_pass, current_password):
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Username or Password mismatch!"'})
+    current_user.hashed_pass = generate_password_hash(new_password, method='sha256')
+    db.session.commit()
+    return jsonify({'message' : 'Successfully changed password, Login again'})
 
+'''
+update a user's role, including promote to director or demote.
+Admin role is required with the logined user.
+Admin role can not be updated by this end point.
+'''
+@app.route('/auth/updateRole', methods=['PUT'])
+@login_required
+def updateRole(current_user):
+    if not current_user.admin:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Admin role required"'})
 
+    target_user_public_id = request.args.get('target_user_public_id')
+    target_user = User.query.filter_by(public_id = target_user_public_id).first()
 
+    if not target_user:
+        return jsonify({'message' : 'Target User can not be found'}), 404
 
-@app.route('/user/updateInfo', methods=['UPDATE'])
-def user_updateInfo():
-    #TODO: for user to update full or part of user info
-    return ''
+    target_user.director = True if request.args.get('operation') == 'promote' else False
+    db.session.commit()
 
-@app.route('/user/newInfo', methods=['POST'])
-def user_newInfo():
-    #TODO: for user to init user info for the first time
-    return ''
+    return jsonify({'message' : 'Successfully Promoted User'})
 
+'''
+update userinfo, create new info if not exit
+'''
+@app.route('/userinfo', methods=['GET','PUT', 'POST'])
+@login_required
+def user_updateInfo(current_user):
+    #create new userinfo
+    if request.method == 'POST':
+        data = request.get_json()
 
+        new_userinfo = UserInfo(user_id = current_user.id, chinese_alias = data['chinese_alias'], english_alias = data['english_alias'], qq = data['qq'])
+        try:
+            db.session.add(new_userinfo)
+            db.session.commit()
+            return jsonify({'message': 'Successfully created userinfo'})
+        except Exception as e:
+            return jsonify({'message' : str(e)}), 400
+        
+    elif request.method == 'PUT':
+        userinfo = UserInfo.query.filter_by(user_id = current_user.id).first()
 
+        if not userinfo:
+            return jsonify({'message' : 'UserInfo isn\'t created'}), 404
 
+        data = request.get_json()
+        new_qq = data['qq']
+        new_chinese_alias = data['chinese_alias']
+        new_english_alias = data['english_alias']
 
+        if new_qq:
+            userinfo.qq = new_qq
+
+        if new_chinese_alias:
+            userinfo.chinese_alias = new_chinese_alias
+
+        if new_english_alias:
+            userinfo.english_alias = new_english_alias
+
+        db.session.commit()
+
+        return jsonify({'message':'Successfully updated userinfo'})
+    elif request.method == 'GET':
+        current_userinfo = UserInfo.query.filter_by(user_id = current_user.id).first()
+
+        if not current_userinfo:
+            return jsonify({'message' : 'userinfo not found'}), 404
+
+        infodata = {}
+        infodata['qq'] = current_userinfo.qq
+        infodata['english_alias'] = current_userinfo.english_alias
+        infodata['chinese_alias'] = current_userinfo.chinese_alias
+
+        return jsonify({'userinfo' : infodata})
 
 
 if __name__ == '__main__':
